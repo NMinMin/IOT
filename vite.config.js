d5469aa9fd02aab85d2a39b0bf90fd1d4c34f9c7
Võ Nhạc Phước <vonhacphuoc@gmail.com>
2026-06-03 19:08:29 +0700

    fix: update base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Thay 'TEN-REPO-CUA-BAN' bằng tên repository thực tế của bạn trên GitHub
-  // Ví dụ: repo của bạn là 'IOT' thì sửa thành base: '/IOT/'
-  // Nếu bạn deploy lên trang cá nhân username.github.io thì xóa dòng base này đi.
-  base: '/TEN-REPO-CUA-BAN/',
+  base: '/IOT/',
 })
